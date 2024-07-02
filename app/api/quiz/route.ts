469efed32a235ebcd2a79288ab95c915f0605be6
sani.usnain <sani.usnain@onescreensolutions.com>
2024-07-02 13:47:47 +0500

    Added logs for quiz route

--- app/api/quiz/route.ts
+++ app/api/quiz/route.ts
@@ -6,6 +6,7 @@ export async function GET(request: Request, response: Response) {
 	const randomNumber = Math.floor(Math.random() * 16);
 
 	response = NextResponse.json(quiz.data[randomNumber]);
+	console.log(response, " ", randomNumber);
 	response.headers.set('Cache-Control', 'no-store');
 	return response;
 }
