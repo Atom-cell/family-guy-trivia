5b981717faa49d7bea3db612e7baa288b779aeb4
sani.usnain <sani.usnain@onescreensolutions.com>
2024-07-02 12:22:55 +0500

    Loading page text centered fixed

--- app/quiz/loading.tsx
+++ app/quiz/loading.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <div className='max-w-screen-md mx-auto flex flex-wrap pt-6 px-2'>Loading...</div>
+    <div className='max-w-screen-md mx-auto flex flex-wrap items-center justify-center pt-6 px-2 text-gray-500'>Loading...</div>
   )
 }
 
