e8ced11d77fde808affd4949828778b8fdfa8e35
sani.usnain <sani.usnain@onescreensolutions.com>
2024-07-02 12:46:00 +0500

    api endpoint

--- app/quiz/question/page.tsx
+++ app/quiz/question/page.tsx
@@ -1,12 +1,12 @@
 'use client'
 
 import React from 'react';
-import { Suspense } from 'react';
+// import { Suspense } from 'react';
 import Question from '@/app/components/Question';
 
 const page = async () => {
 	return (
-		<main className='max-w-screen-md mx-auto flex flex-col flex-wrap pt-6 px-4'>
+		<main className='max-w-screen-md mx-auto flex flex-col flex-wrap pt-6 px-4 '>
 			<Question />
 		</main>
 	);
